193248369220e5cd8d4a106d139c42c0643ec2e6
JulianChu <yulang.chu@gmail.com>
2018-04-13 23:35:04 +0200

    fix read json

--- chapter3/test-currency.js
+++ chapter3/test-currency.js
@@ -1,5 +1,7 @@
-var currency = require('./currency');
-
+var Currency = require('./currency2');
+var currency = new Currency(0.91);
+console.log(Currency);
+console.log(currency);
 console.log('50 Canadia dollars equals this amount of US dollers:');
 console.log(currency.canadianToUS(50));
 
